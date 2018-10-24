4c7e282264da880c2c4f612efdf9112601fd6873
usw344 <usw873pk@gmail.com>
2018-10-24 13:04:33 -0600

    Update index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<header>
+<title>My website</title>
+<style>
+
+p{
+    color:blue;
+}
+
+
+</style>
+
+</header>
+
+<body>
+
+<p>Lorem hello world</p>
+
+</body>
+
+
+
+
+
+</html>
